9938b145a34c85abcaa2827786108ff5120c2171
jinl33 <jinl33@nycstudents.net>
2025-11-03 13:57:36 -0500

    Link CSS JS, Basic Logic JS

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 
+
+
+
+let Min_Number = 0;
+let Max_Number = 1000;
+
+
+
+let Number_To_Guess = 0;
+
+
+function Randomize_The_Guess_Number(){
+    Number_To_Guess = Math.floor(Math.random() * (Max_Number - Min_Number + 1)) + Min_Number;
+}
+
+function Even_Or_Odd(Input_Number){
+    if (Input_Number % 2 == 0){
+        return "Even";
+    }
+    if (Input_Number % 2 !== 0){
+        return "Odd";
+    } else {
+        return "Neither";
+    }
+    
+}
+function Check_Answer(Input_Number){
+    if (Input_Number < Number_To_Guess){
+        return "Too Low";
+    }
+    if (Input_Number == Number_To_Guess){
+        return "Correct";
+    }
+    if (Input_Number > Number_To_Guess){
+        return "Too High";
+    }
+}
+
+Randomize_The_Guess_Number();
+console.log(Number_To_Guess);
+console.log(Even_Or_Odd(Number_To_Guess));
