76aaf8484d992205804bc9956ac7727db41efd7e
jinfeiliang <tinfeek80@gmail.com>
2025-11-09 16:02:21 -0500

    HTML Display Stats and Bold CSS

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ let Max_Number = 100;
 let Number_To_Guess = 0;
 
 let Stats = {
-    Tries_This_Round: 0,
-    Wins: 0,
-    Losses: 0,
-    Rounds: 0,
-    Streak: 0,
-    Retries: 0,
-}
+  Tries_This_Round: 0,
+  Wins: 0,
+  Losses: 0,
+  Rounds: 0,
+  Streak: 0,
+  Retries: 0,
+};
 
 function Randomize_The_Guess_Number() {
   Number_To_Guess =
     Math.floor(Math.random() * (Max_Number - Min_Number + 1)) + Min_Number;
+  console.log(Number_To_Guess);
 }
 
 function Even_Or_Odd(Input_Number) {
@@ -39,32 +40,78 @@ function Check_Answer(Input_Number) {
   }
 }
 
-let Game_Comment_Text_DOM = document.querySelector(".Game_Comment_Text");
+function Update_All_Display_Stats() {
+  document.querySelector("#Game_Current_Tries").innerHTML =
+    Stats.Tries_This_Round;
+  document.querySelector("#Game_Stat_Wins_Value").innerHTML = Stats.Wins;
+  document.querySelector("#Game_Stat_Losses_Value").innerHTML = Stats.Losses;
+  document.querySelector("#Game_Stat_Rounds_Value").innerHTML = Stats.Rounds;
+  document.querySelector("#Game_Stat_Streak_Value").innerHTML = Stats.Streak;
+  document.querySelector("#Game_Stat_Retries_Value").innerHTML = Stats.Retries;
+}
 
+function Reset_All_Stats() {
+  for (let index in Stats) {
+    Stats[index] = 0;
+  }
+  Update_All_Display_Stats();
+}
+document
+  .querySelector("#Game_Reset_All_BTN")
+  .addEventListener("click", Reset_All_Stats);
+
+function Give_Up() {
+  Stats.Losses += 1;
+  Update_All_Display_Stats();
+}
+document.querySelector("#Game_Give_Up_BTN").addEventListener("click", Give_Up);
+
+function Retry() {
+  Randomize_The_Guess_Number();
+  Stats.Retries += 1;
+  Update_All_Display_Stats();
+}
+document.querySelector("#Game_Retry_BTN").addEventListener("click", Retry);
+
+let Game_Comment_Text_DOM = document.querySelector(".Game_Comment_Text");
 function Guess_Answer(Guess) {
   let Numbered_Guess = Number(Guess);
   let Status = Check_Answer(Numbered_Guess);
-
+  let Hint_Activation = document.querySelector("#Game_Hint_Input").checked;
+  console.log(Hint_Activation);
   switch (Status) {
     case "Too Low":
       console.log("Too Low");
-      Game_Comment_Text_DOM.innerHTML = `Your number is too low, try an higher number. The answer is ${Even_Or_Odd(
-        Number_To_Guess
-      )}`;
+      if (Hint_Activation) {
+        Game_Comment_Text_DOM.innerHTML = `Your number is too <span class="bold">low</span>, try an <span class="bold">higher</span> number. The answer is <span class="bold">${Even_Or_Odd(
+          Number_To_Guess
+        )}</span>`;
+      } else {
+        Game_Comment_Text_DOM.innerHTML = `Your number is too <span class="bold">low</span>, try an <span class="bold">higher</span> number`;
+      }
+
       break;
 
     case "Correct":
       console.log("Correct");
       Game_Comment_Text_DOM.innerHTML = `Correct answer 👏👏👏`;
+      Stats.Wins += 1;
+      Randomize_The_Guess_Number();
       break;
 
     case "Too High":
       console.log("Too High");
-      Game_Comment_Text_DOM.innerHTML = `Your number is too high, try an lower number. The answer is ${Even_Or_Odd(
-        Number_To_Guess
-      )}`;
+      if (Hint_Activation) {
+        Game_Comment_Text_DOM.innerHTML = `Your number is too high, try an lower number. The answer is ${Even_Or_Odd(
+          Number_To_Guess
+        )}`;
+      } else {
+        Game_Comment_Text_DOM.innerHTML = `Your number is too high, try an lower number`;
+      }
       break;
   }
+  Stats.Tries_This_Round += 1;
+  Update_All_Display_Stats();
 }
 
 // Conectar
@@ -97,4 +144,3 @@ document
 
 // Default?
 Randomize_The_Guess_Number();
-console.log(Number_To_Guess)
